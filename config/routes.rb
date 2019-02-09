3892b568d2896cdaec52833f8d57e6a44f219d29
April Dagonese <adagonese@gmail.com>
2019-02-09 15:56:02 -0700

    Adds nav bar tests and code
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>
    Co-authored-by: Zach Nager <znager@gmail.com>
    Co-authored-by: Peregrine Balas <39976770+PeregrineReed@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'welcome'
+root 'welcome#index'
 
 namespace :admin do
 	resources :users
@@ -19,6 +19,8 @@ namespace :visitor do
 	resources :users
   resources :items
 end
+
+resources :carts, only: [:create]
 
 get '/login', to: 'sessions#new'
 post '/login', to: 'sessions#create'
