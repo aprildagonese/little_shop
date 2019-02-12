c87a7f3cb80b37a024f9687f1c9bc83e3964610f
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-12 12:00:53 -0700

    Wrote edge case tests for login

--- spec/features/visitor/login_spec.rb
+++ spec/features/visitor/login_spec.rb
@@ -31,8 +31,6 @@ RSpec.describe 'as a visitor' do
     expect(page).to have_content("Welcome, #{name}!")
   end
 
-
-
   it 'errors and refreshes if credentials bad' do
     user = User.create(name: "funbucket13", email: "funbucket13@gmail.com", password: "test")
 
@@ -56,14 +54,61 @@ RSpec.describe 'as a visitor' do
     ##Add expect error flash messages
   end
 
-  it 'can log in as a merchant' do
-    user = User.create(name: "funbucket13", email: "funbucket13@gmail.com", password: "test", role: 1)
+
+
+  it 'errors and refreshes if log in attempt with no account' do
+    email = "funbucket13@gmail.com"
+    password = "test"
 
     visit items_path
 
     click_on "I already have an account"
 
     expect(current_path).to eq(login_path)
+
+    fill_in "email", with: email
+    fill_in "password", with: password
+
+    click_button "Log In"
+
+    expect(current_path).to eq(login_path)
+
+    expect(page).to have_selector("input[type=submit][value='Log In']")
+    expect(page).to have_content("Failed to log you in, please check name and email.")
+  end
+
+
+
+  it 'errors and refreshes if log in attempt without both name and password' do
+    email = "funbucket13@gmail.com"
+    password = "test"
+
+    visit login_path
+
+    fill_in "email", with: email
+
+    click_button "Log In"
+
+    expect(current_path).to eq(login_path)
+    
+    expect(page).to have_selector("input[type=submit][value='Log In']")
+    expect(page).to have_content("Failed to log you in, please check name and email.")
+
+    fill_in "password", with: password
+
+    expect(current_path).to eq(login_path)
+
+    expect(page).to have_selector("input[type=submit][value='Log In']")
+    expect(page).to have_content("Failed to log you in, please check name and email.")
+  end
+
+
+
+  it 'can log in as a merchant' do
+    user = User.create(name: "funbucket13", email: "funbucket13@gmail.com", password: "test", role: 1)
+
+    visit login_path
+
     fill_in "email", with: user.email
     fill_in "password", with: user.password
 
@@ -80,11 +125,8 @@ RSpec.describe 'as a visitor' do
   it 'can log in as an admin' do
     user = User.create(name: "funbucket13", email: "funbucket13@gmail.com", password: "test", role: 2)
 
-    visit items_path
+    visit login_path
 
-    click_on "I already have an account"
-
-    expect(current_path).to eq(login_path)
     fill_in "email", with: user.email
     fill_in "password", with: user.password
 
@@ -101,11 +143,8 @@ RSpec.describe 'as a visitor' do
   it 'can log in as a user' do
     user = User.create(name: "funbucket13", email: "funbucket13@gmail.com", password: "test")
 
-    visit items_path
+    visit login_path
 
-    click_on "I already have an account"
-
-    expect(current_path).to eq(login_path)
     fill_in "email", with: user.email
     fill_in "password", with: user.password
 
@@ -118,25 +157,4 @@ RSpec.describe 'as a visitor' do
     expect(page).to have_link("Log Out")
   end
 
-
-
-  it 'errors and refreshes if log in attempt with no account' do
-    email = "funbucket13@gmail.com"
-    password = "test"
-
-    visit items_path
-
-    click_on "I already have an account"
-
-    expect(current_path).to eq(login_path)
-    fill_in "email", with: email
-    fill_in "password", with: password
-
-    click_button "Log In"
-
-    expect(current_path).to eq(login_path)
-
-    expect(page).to have_selector("input[type=submit][value='Log In']")
-    expect(page).to have_content("Failed to log you in, please check name and email.")
-  end
 end
