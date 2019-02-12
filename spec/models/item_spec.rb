143bff8898d6297a5fec31e27880406074ad99a3
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-11 17:34:43 -0700

    Uncommented shoulda's for item relationships
    Co-authored-by: Scott Thomas <31359242+smthom05@users.noreply.github.com>
    Co-authored-by: April Dagonese <adagonese@gmail.com>
    Co-authored-by: Zach Nager <znager@gmail.com>

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Item, type: :model do
-  # describe "relationships" do
-  #   it {should have_many :}
-  #   it {should have_many(:).through(:)}
-  #   it {should belong_to(:)}
-  # end
+  describe "relationships" do
+    it {should have_many :order_items}
+    it {should have_many(:orders).through :order_items}
+    it {should belong_to :user}
+  end
 
   describe 'Validations' do
     describe 'Required Field(s)' do
