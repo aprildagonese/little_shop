a5e41f83e4b6c8899aa7d4ecc1cc8fcc87739375
Scott Thomas <smthom05@gmail.com>
2019-02-13 14:06:01 -0700

    updated bundler to be 2.0 or higher

--- spec/features/visitor_can_add_items_to_their_cart_spec.rb
+++ spec/features/visitor_can_add_items_to_their_cart_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe 'From an items show page' do
       expect(page).to have_content("Cart: 1")
     end
   end
+
+  context 'as a registered user' do
+    it 'can add an item to their cart' do
+      user = create(:user, password: 'test', username: '')
+      visit item_path(@item)
+
+      click_button "Add Item To Cart"
+
+      expect(current_path).to eq(items_path)
+      expect(page).to have_content("1 #{@item.title} has been added to your cart.")
+      expect(page).to have_content("Cart: 1")
+    end
+  end
 end
