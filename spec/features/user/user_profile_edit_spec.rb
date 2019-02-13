b52640f3a1f8aa3737c0f3cd05b2fb423ffa3812
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-13 15:19:33 -0700

    Updated profile edit feature test

--- spec/features/user/user_profile_edit_spec.rb
+++ spec/features/user/user_profile_edit_spec.rb
@@ -3,16 +3,28 @@ require 'rails_helper'
 RSpec.describe 'as a registered user' do
 
   before :each do
-    @user = create(:user)
+    @user_1 = create(:user)
+    @user_2 = create(:user)
   end
 
   it 'can\'t view form unless logged in' do
+
   end
 
   it 'sees a form to edit profile info' do
-    login_as(@user)
+
+    login_as(@user_1)
+
     visit profile_edit_path
-    save_and_open_page
+
+    expect(page).to have_content("Name")
+    expect(page).to have_content("Street address")
+    expect(page).to have_content("City")
+    expect(page).to have_content("State")
+    expect(page).to have_content("Zip code")
+    expect(page).to have_content("Email")
+    expect(page).to have_content("Password")
+    expect(page).to have_content("Password confirmation")
 
   end
 
