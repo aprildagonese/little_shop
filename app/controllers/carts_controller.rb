586beda8c8e4874330351015cb7ea5335be240bd
Scott Thomas <smthom05@gmail.com>
2019-02-13 14:35:26 -0700

    model test for add_item and total count working for cart spec

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,7 @@
 class CartsController < ApplicationController
   def create
     item = Item.find(params[:item_id])
+    @cart.add_item(item.id)
     flash[:notice] = "1 #{item.title} has been added to your cart."
     redirect_to items_path
   end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,15 @@
 class Cart
+  attr_reader :contents
 
   def initialize(contents)
-    @contents = contents || Hash.new()
+    @contents = contents || Hash.new(0)
   end
 
   def total_count
     @contents.values.sum
+  end
+
+  def add_item(id)
+    @contents[id] += 1
   end
 end
