5a1a85ad88f81860754a37db0819bb1468e52b74
April Dagonese <41272635+aprildagonese@users.noreply.github.com>
2019-02-12 19:18:07 -0700

    Update order_items.rb

--- spec/factories/order_items.rb
+++ spec/factories/order_items.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     item
     order
     sale_price { Faker::Number.decimal(2, 2) }
-    quantity { Faker::Number.decimal(1, 0)}
+    quantity { Faker::Number.number(2) }
   end
 end
