660ec8cfe5530a30e3a4d330a4f02ac4d5fa6266
April Dagonese <adagonese@gmail.com>
2019-02-13 18:25:58 -0700

    more tests

--- spec/features/admin/admin_sees_merchants_index.rb
+++ spec/features/admin/admin_sees_merchants_index.rb
@@ -29,7 +29,6 @@ RSpec.describe "As an admin" do
       expect(page).to have_content("#{merch_2.city}")
       expect(page).to have_content("#{merch_2.state}")
       expect(page).to have_content("#{merch_2.created_at.to_date.to_s}")
-      expect(page).to have_button("Deactivate")
       expect(page).to_not have_content("#{merch_1.name}")
       expect(page).to_not have_content("#{merch_1.city}")
       expect(page).to_not have_content("#{merch_3.name}")
@@ -40,7 +39,6 @@ RSpec.describe "As an admin" do
       expect(page).to have_content("#{merch_3.city}")
       expect(page).to have_content("#{merch_3.state}")
       expect(page).to have_content("#{merch_3.created_at.to_date.to_s}")
-      expect(page).to have_button("Deactivate")
       expect(page).to_not have_content("#{merch_2.name}")
       expect(page).to_not have_content("#{merch_2.city}")
       expect(page).to_not have_content("#{merch_1.name}")
@@ -48,7 +46,7 @@ RSpec.describe "As an admin" do
     end
   end
 
-  it "displays activation button correctly by activation_status" do
+  it "toggles merchant activation_status" do
     admin_user = User.create!(name: "Admina", email: "admin@email.com", password: "admintest", role: 2)
     merch_1 = User.create!(name: "Merch1", email: "merch1@email.com", password: "merchtest", role: 1, activation_status: 0)
     merch_2 = User.create!(name: "Merch2", email: "merch2@email.com", password: "merchtest", role: 1, activation_status: 0)
@@ -56,4 +54,14 @@ RSpec.describe "As an admin" do
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
     visit admin_merchants_path
+
+    within ".Merch1-row" do
+      expect(page).to have_button("Deactivate")
+    end
+    within ".Merch2-row" do
+      expect(page).to have_button("Deactivate")
+    end
+    within ".Merch3-row" do
+      expect(page).to have_button("Activate")
+    end
 end
