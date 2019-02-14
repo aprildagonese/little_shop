9a4a0bcf741c916decfe6510a812bd107d2b3198
nagerz <znager@gmail.com>
2019-02-14 11:00:20 -0700

    Added images for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all.where(activation_status: "active")
+    @items = Item.where(activation_status: "active")
   end
 
   def show
