0b344c59f0903b7b3e41aa81fb5f5681a9509079
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-14 20:10:23 -0700

    Updated orders controller index action to have user variable

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -2,9 +2,7 @@ class User::OrdersController < ApplicationController
   before_action :require_current_user
 
   def index
-    #Didn't actually need this yet for my story so commenting out.
-    #Check for accuracy
-    #@orders = Order.where(user_id: current_user)
+    @user = User.find(session[:user_id])
   end
 
   def show
