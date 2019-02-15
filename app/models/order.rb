e38501ba0b04fc4d6226908deaf6fe6ff2c76585
April Dagonese <adagonese@gmail.com>
2019-02-14 17:31:19 -0700

    Updates orders table for status

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items#, dependent: :delete_all
   has_many :items, through: :order_items#, dependent: :delete_all
+
+  enum status: [:pending, :fulfilled, :cancelled]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190213004046) do
+ActiveRecord::Schema.define(version: 20190215002748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,10 +40,10 @@ ActiveRecord::Schema.define(version: 20190213004046) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string "activation_status"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
