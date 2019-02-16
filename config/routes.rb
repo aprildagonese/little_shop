fb0e6a599cca17af6bdd03e8cce584e91c8a9fc6
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-15 23:07:13 -0700

    Added patch route for cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,43 @@
 Rails.application.routes.draw do
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'welcome#index', as: "welcome"
-
-resources :merchants, only: [:index, :update]
-
-resources :items, only: [:show, :index, :edit, :new, :destroy]
-
-namespace :merchant do
-  resources :items, except: [:show]
-end
-
-get '/cart', to: 'carts#show'
-post '/cart', to: 'carts#create', as: 'carts'
-
-resources :users, only: [:show, :index, :create, :update] do
-  resources :orders, only: [:show]
-end
-
-namespace :admin do
-  resources :merchants, only: [:show, :index]
-  resources :items, except: [:show]
-  resources :users, only: [:show, :index, :edit, :update]
-end
-
 get '/register', to: 'users#new'
-# get '/merchants', to: 'users#index'
-get '/merchants/:id', to: 'users#show'
-
 get '/login', to: 'sessions#new'
 post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy'
 
-get '/dashboard', to: 'merchants#show'
-get '/admin/dashboard', to: 'admin/dashboard#show'
-get '/dashboard/items', to: 'merchant/items#index'
+resources :items, only: [:show, :index, :edit, :new, :destroy]
+get '/cart', to: 'carts#show'
+post '/cart', to: 'carts#create', as: 'carts'
+patch '/cart', to: 'carts#update'
+
+#------------User---------------
 get '/profile', to: 'users#show'
 get '/profile/edit', to: 'users#edit'
 put '/profile', to: 'users#update'
 get '/profile/orders', to: 'user/orders#index'
 patch '/profile/orders', to: 'user/orders#update'
 get '/profile/orders/:id', to: 'user/orders#show'
+resources :users, only: [:show, :index, :create, :update] do
+  resources :orders, only: [:show]
+end
+
+#------------Merchant-------------
+get '/dashboard', to: 'merchants#show'
+get '/dashboard/items', to: 'merchant/items#index'
+# get '/merchants', to: 'users#index'
+resources :merchants, only: [:index, :update]
+namespace :merchant do
+  resources :items, except: [:show]
+end
+get '/merchants/:id', to: 'users#show' #???
+
+#--------------Admin---------------
+get '/admin/dashboard', to: 'admin/dashboard#show'
+namespace :admin do
+  resources :merchants, only: [:show, :index]
+  resources :items, except: [:show]
+  resources :users, only: [:show, :index, :edit, :update]
+end
+
 
 end
