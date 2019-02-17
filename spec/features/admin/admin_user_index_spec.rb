868db4c2739eb1d1ab83ec67e6f5d27a9059a22b
Scott Thomas <smthom05@gmail.com>
2019-02-17 10:21:46 -0700

    resolved merge conflicts on local machine

--- spec/features/admin/admin_user_index_spec.rb
+++ spec/features/admin/admin_user_index_spec.rb
@@ -5,17 +5,10 @@ RSpec.describe "As an admin", type: :feature do
   before :each do
     Faker::UniqueGenerator.clear
 
-<<<<<<< HEAD
-    @admin = create(:user, role: 2)
-    login_as(@admin)
-    @merchant = create(:user, role: 1)
-    @admin_2 = create(:user, role: 2)
-=======
     @admin = create(:user, name: "IAmAdmin", role: 2)
     login_as(@admin)
     @merchant = create(:user, name: "IAmMerchant", role: 1)
     @admin_2 = create(:user, name: "IAmAdmin2", role: 2)
->>>>>>> 9a80fd69e4d6dc61adf3546bb456726e14907273
     @user_1 = create(:user, role: 0)
     @user_2 = create(:user, role: 0)
     @user_3 = create(:user, role: 0)
@@ -24,23 +17,13 @@ RSpec.describe "As an admin", type: :feature do
     @user_6 = create(:user, role: 0, activation_status: 1)
   end
 
-<<<<<<< HEAD
-  context "when it clicks on the User's link in the nav" do
-=======
+
   context "when it clicks on the Users link in the nav" do
->>>>>>> 9a80fd69e4d6dc61adf3546bb456726e14907273
     it 'shows all users in the system who are not merchants nor admins' do
       visit welcome_path
 
       click_link "Users"
 
-<<<<<<< HEAD
-      expect(current_path).to eq(admin_users)
-
-      expect(page).to_have("All Users")
-
-
-=======
       expect(current_path).to eq(admin_users_path)
 
       expect(page).to have_content("All Users")
@@ -78,19 +61,9 @@ RSpec.describe "As an admin", type: :feature do
 
       expect(page).to_not have_content(@admin_2.name)
       expect(page).to_not have_content(@merchant.name)
->>>>>>> 9a80fd69e4d6dc61adf3546bb456726e14907273
-
     end
 
     it "all user's names are links to their show pages" do
-<<<<<<< HEAD
-    end
-
-    it "next to each user's name is the date they registered" do
-    end
-
-    it "next to each user's name is a button that says 'enable' or 'disable' based on their current status" do
-=======
       visit admin_users_path
 
       click_link @user_1.name
@@ -100,7 +73,7 @@ RSpec.describe "As an admin", type: :feature do
 
     it "next to each user's name is the date they registered and a button to enable or disable based on current activation status" do
       visit admin_users_path
-      
+
       within ".user-#{@user_1.id}-row" do
         expect(page).to have_content(@user_1.created_at.to_date.to_s)
         expect(page).to have_button("Disable")
@@ -125,7 +98,6 @@ RSpec.describe "As an admin", type: :feature do
         expect(page).to have_content(@user_6.created_at.to_date.to_s)
         expect(page).to have_button("Enable")
       end
->>>>>>> 9a80fd69e4d6dc61adf3546bb456726e14907273
     end
   end
 end
