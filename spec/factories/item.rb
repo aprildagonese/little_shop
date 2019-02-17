e2870b36a0aeefd9ce80334a4379b730d3031c4e
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-17 13:29:14 -0700

    Created aliases for item and user factories

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,11 +1,15 @@
 FactoryBot.define do
-  factory :item do
+  factory :item, class: Item do
     user
     title { Faker::Food.unique.dish }
     description { Faker::Food.description }
     quantity { Faker::Number.number(4) }
     price { Faker::Number.decimal(2, 2) }
-    activation_status { "active" }
+    active { true }
     image_url { 'https://cdn.shopify.com/s/files/1/0004/8132/9204/products/gummi-pickle1_2_1_1_1024x1024.jpg?v=1522430300' }
+  end
+
+  factory :disabled_item, parent: :item do
+    active { false }
   end
 end
