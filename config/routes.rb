69bbdb1eaef0cf0e6a0aeac9603984d06e758a86
April Dagonese <41272635+aprildagonese@users.noreply.github.com>
2019-02-17 15:20:30 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ get '/profile/edit', to: 'users#edit'
 put '/profile', to: 'users#update'
 get '/profile/orders', to: 'user/orders#index'
 patch '/profile/orders', to: 'user/orders#update'
-get '/profile/orders/:id', to: 'user/orders#show', as: 'profile/order'
+get '/profile/orders/:id', to: 'user/orders#show', as: 'profile_order'
 resources :users, only: [:show, :index, :create, :update] do
   resources :orders, only: [:show, :create]
 end
