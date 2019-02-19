91e9799ace422e44a10df7c6483b1925bb9ef85a
April Dagonese <adagonese@gmail.com>
2019-02-19 16:02:36 -0700

    Adds working ability to fulfill order items

--- app/controllers/merchants/order_items_controller.rb
+++ app/controllers/merchants/order_items_controller.rb
@@ -1,6 +1,9 @@
 class Merchants::OrderItemsController < Merchants::BaseController
 
   def update
-    binding.pry
+    order_item = OrderItem.find(params[:id])
+    order_item.fulfill_order_item
+    flash[:success] = "Your item has been fulfilled."
+    redirect_to dashboard_order_path(order_item.order)
   end
 end
--- spec/features/merchant/merchant_fulfills_order_items_spec.rb
+++ spec/features/merchant/merchant_fulfills_order_items_spec.rb
@@ -1,28 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "as a merchant" do
-  before :each do
-    Faker::UniqueGenerator.clear
-    @user = create(:user)
-    @merchant = create(:user, role: 1)
-    @merchant_2 = create(:user, role: 1)
-    @order = create(:order, user: @user)
-    @i1, @i2, @i3 = create_list(:item, 3, user: @merchant, quantity: 5, price: 5)
-    @i4, @i5 = create_list(:item, 2, user: @merchant_2, quantity: 5, price: 5)
-    @oi1, @oi2, @oi3, @oi4, @oi5 = create_list(:order_item, 5)
-    @oi1.update(order: @order, item: @i1, sale_price: 2, quantity: 5)
-    @oi2.update(order: @order, item: @i2, sale_price: 3, quantity: 5, fulfillment_status: 1)
-    @oi3.update(order: @order, item: @i3, sale_price: 4, quantity: 3)
-    @oi4.update(order: @order, item: @i4, sale_price: 5, quantity: 6)
-    @oi5.update(order: @order, item: @i5, sale_price: 6, quantity: 6)
-    @order_items = [@oi1, @oi2, @oi3, @oi4, @oi5]
-
-    login_as(@merchant)
-    visit dashboard_order_path(@order)
-  end
-
   context "if the item has already been fulfilled" do
     it "it is listed as fulfilled" do
+      Faker::UniqueGenerator.clear
+      @user = create(:user)
+      @merchant = create(:user, role: 1)
+      @merchant_2 = create(:user, role: 1)
+      @order = create(:order, user: @user)
+      @i1, @i2, @i3 = create_list(:item, 3, user: @merchant, quantity: 5, price: 5)
+      @i4, @i5 = create_list(:item, 2, user: @merchant_2, quantity: 5, price: 5)
+      @oi1, @oi2, @oi3, @oi4, @oi5 = create_list(:order_item, 5)
+      @oi1.update(order: @order, item: @i1, sale_price: 2, quantity: 5)
+      @oi2.update(order: @order, item: @i2, sale_price: 3, quantity: 5, fulfillment_status: 1)
+      @oi3.update(order: @order, item: @i3, sale_price: 4, quantity: 3)
+      @oi4.update(order: @order, item: @i4, sale_price: 5, quantity: 6)
+      @oi5.update(order: @order, item: @i5, sale_price: 6, quantity: 6)
+      @order_items = [@oi1, @oi2, @oi3, @oi4, @oi5]
+
+      login_as(@merchant)
+      visit dashboard_order_path(@order)
+
       within ".item-#{@oi1.id}" do
         expect(page).to have_button("Fulfill Item")
       end
@@ -36,6 +34,24 @@ RSpec.describe "as a merchant" do
   end
   context "when I do not enough in-stock quantity" do
     it 'I can not fulfill an order-item' do
+      Faker::UniqueGenerator.clear
+      @user = create(:user)
+      @merchant = create(:user, role: 1)
+      @merchant_2 = create(:user, role: 1)
+      @order = create(:order, user: @user)
+      @i1, @i2, @i3 = create_list(:item, 3, user: @merchant, quantity: 5, price: 5)
+      @i4, @i5 = create_list(:item, 2, user: @merchant_2, quantity: 5, price: 5)
+      @oi1, @oi2, @oi3, @oi4, @oi5 = create_list(:order_item, 5)
+      @oi1.update(order: @order, item: @i1, sale_price: 2, quantity: 5)
+      @oi2.update(order: @order, item: @i2, sale_price: 3, quantity: 5, fulfillment_status: 1)
+      @oi3.update(order: @order, item: @i3, sale_price: 4, quantity: 3)
+      @oi4.update(order: @order, item: @i4, sale_price: 5, quantity: 6)
+      @oi5.update(order: @order, item: @i5, sale_price: 6, quantity: 6)
+      @order_items = [@oi1, @oi2, @oi3, @oi4, @oi5]
+
+      login_as(@merchant)
+      visit dashboard_order_path(@order)
+
       within ".item-#{@oi4.id}" do
         expect(page).to_not have_button("Fulfill Item")
       end
@@ -44,36 +60,48 @@ RSpec.describe "as a merchant" do
       end
     end
   end
+
   context "when I have the quantity in stock, I can fulfill an item" do
     it "by changing the oi status and reducing the item's quantity" do
+      Faker::UniqueGenerator.clear
+      @user = create(:user)
+      @merchant = create(:user, role: 1)
+      @merchant_2 = create(:user, role: 1)
+      @order = create(:order, user: @user)
+      @i1, @i2, @i3 = create_list(:item, 3, user: @merchant, quantity: 5, price: 5)
+      @i4, @i5 = create_list(:item, 2, user: @merchant_2, quantity: 5, price: 5)
+      @oi1, @oi2, @oi3, @oi4, @oi5 = create_list(:order_item, 5)
+      @oi1.update(order: @order, item: @i1, sale_price: 2, quantity: 5)
+      @oi2.update(order: @order, item: @i2, sale_price: 3, quantity: 5, fulfillment_status: 1)
+      @oi3.update(order: @order, item: @i3, sale_price: 4, quantity: 3)
+      @oi4.update(order: @order, item: @i4, sale_price: 5, quantity: 6)
+      @oi5.update(order: @order, item: @i5, sale_price: 6, quantity: 6)
+      @order_items = [@oi1, @oi2, @oi3, @oi4, @oi5]
+
+      login_as(@merchant)
+      visit dashboard_order_path(@order)
+
       within ".item-#{@oi1.id}" do
         click_button "Fulfill Item"
+      end
         expect(current_path).to eq(dashboard_order_path(@order))
-        expect(page).to have_content("Fulfilled")
         expect(@oi1.item.quantity).to eq(0)
         expect(@oi1.fulfilled?).to eq(true)
-      end
-      within ".item-#{@oi3.id}" do
-        click_button "Fulfill Item"
-        expect(current_path).to eq(dashboard_order_path(@order))
-        expect(page).to have_content("Fulfilled")
-        expect(@oi3.item.quantity).to eq(2)
-        expect(@oi3.fulfilled?).to eq(true)
+        expect(page).to have_content("Your item has been fulfilled.")
+      within ".item-#{@oi1.id}" do
+        expect(page).to have_content("Order Item Status: Fulfilled")
       end
 
+      within ".item-#{@oi3.id}" do
+        click_button "Fulfill Item"
+      end
+        expect(current_path).to eq(dashboard_order_path(@order))
+        expect(@oi3.item.quantity).to eq(2)
+        expect(@oi3.fulfilled?).to eq(true)
+        expect(page).to have_content("Your item has been fulfilled.")
+      within ".item-#{@oi3.id}" do
+        expect(page).to have_content("Order Item Status: Fulfilled")
+      end
     end
   end
 end
-
-
-
-#       When I visit an order show page from my dashboard
-# For each item of mine in the order
-# If the user's desired quantity is equal to or less than my current inventory quantity for that item
-# And I have not already "fulfilled" that item:
-# - Then I see a button or link to "fulfill" that item
-# - When I click on that link or button I am returned to the order show page
-# - I see the item is now fulfilled
-# - I also see a flash message indicating that I have fulfilled that item
-# - My inventory quantity is permanently reduced by the user's desired quantity
-#If I have already fulfilled this item, I see text indicating such.
