cb2dac638b08d6864f44921d12a87bc3e3f0550a
Scott Thomas <smthom05@gmail.com>
2019-02-19 15:50:02 -0700

    fixed top 5 items test with correct items. Top items, cities, and states tests still need to take into account the order of whats on the page

--- spec/features/merchant/merchant_visits_dashboard_spec.rb
+++ spec/features/merchant/merchant_visits_dashboard_spec.rb
@@ -75,11 +75,11 @@ RSpec.describe "as a merchant" do
       @order9 = create(:order, user: @user8)
       @oi1 = create(:fulfilled_order_item, order: @order1, item: @item1, quantity: 1)
       @oi2 = create(:fulfilled_order_item, order: @order2, item: @item2, quantity: 2)
-      @oi3 = create(:order_item, order: @order3, item: @item3, quantity: 3)
-      @oi4 = create(:order_item, order: @order4, item: @item4, quantity: 4)
-      @oi5 = create(:order_item, order: @order5, item: @item5, quantity: 5)
-      @oi6 = create(:order_item, order: @order6, item: @item6, quantity: 6)
-      @oi7 = create(:order_item, order: @order7, item: @item7, quantity: 7)
+      @oi3 = create(:order_item, order: @order3, item: @item3, quantity: 30)
+      @oi4 = create(:order_item, order: @order4, item: @item4, quantity: 40)
+      @oi5 = create(:order_item, order: @order5, item: @item5, quantity: 50)
+      @oi6 = create(:order_item, order: @order6, item: @item6, quantity: 60)
+      @oi7 = create(:order_item, order: @order7, item: @item7, quantity: 70)
       @oi8 = create(:fulfilled_order_item, order: @order2, item: @item8, quantity: 2)
       @oi9 = create(:fulfilled_order_item, order: @order2, item: @item2, quantity: 2)
       @oi10 = create(:fulfilled_order_item, order: @order9, item: @item8, quantity: 2)
@@ -90,11 +90,13 @@ RSpec.describe "as a merchant" do
 
     it "top 5 items sold by quantity" do
        # TODO: Check order
-      expect(page).to have_content("#{@item7.title}: #{@item7.units_sold} units sold")
-      expect(page).to have_content("#{@item6.title}: #{@item6.units_sold} units sold")
-      expect(page).to have_content("#{@item5.title}: #{@item5.units_sold} units sold")
-      expect(page).to have_content("#{@item4.title}: #{@item4.units_sold} units sold")
-      expect(page).to have_content("#{@item3.title}: #{@item3.units_sold} units sold")
+       within '#top-items' do
+        expect(page).to have_content("#{@item7.title}: #{@item7.units_sold} units sold")
+        expect(page).to have_content("#{@item6.title}: #{@item6.units_sold} units sold")
+        expect(page).to have_content("#{@item5.title}: #{@item5.units_sold} units sold")
+        expect(page).to have_content("#{@item4.title}: #{@item4.units_sold} units sold")
+        expect(page).to have_content("#{@item3.title}: #{@item3.units_sold} units sold")
+      end
     end
 
     it "total quantity of items sold" do
@@ -102,7 +104,7 @@ RSpec.describe "as a merchant" do
     end
 
     it 'sees top 3 states and quantities' do
-      within '#top_states' do
+      within '#top-states' do
         expect(page).to have_content("Top 3 States Where Items Were Shipped")
         expect(page).to have_content("MO (6 items shipped)")
         expect(page).to have_content("PA (2 items shipped)")
@@ -111,10 +113,12 @@ RSpec.describe "as a merchant" do
     end
 
     it 'sees top 3 city, states and their quantities' do
-      save_and_open_page
-      within '#top_city_states' do
+      within '#top-cities' do
         expect(page).to have_content("Top 3 Cities Where Items Were Shipped")
         expect(page).to have_content("Springfield, MO (6 items shipped)")
+        expect(page).to have_content("Harrisburg, PA (2 items shipped)")
+        expect(page).to have_content("Springfield, IL (1 item shipped)")
+
       end
     end
   end
