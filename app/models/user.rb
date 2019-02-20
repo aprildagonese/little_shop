278d734f51e4eb6fa8433454081d7e126d15eaa3
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-02-19 18:38:09 -0700

    Moved flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password, require: true
-  #validates :password_confirmation, presence: true
-
+  
   has_secure_password
 
   enum role: [:registered, :merchant, :admin]
